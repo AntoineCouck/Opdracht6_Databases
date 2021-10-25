7d40649396f19361d9def0f3ed8e6f7e21cf835e
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-10-25 22:06:50 +0200

    Add project files.

---
+++ Oef1/db/dbo.Transactie.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Transacties]
+(
+	[AandeelId] INT NOT NULL PRIMARY KEY IDENTITY (1,1), 
+    [AantalGekocht] INT NOT NULL DEFAULT 0, 
+    [Koers] MONEY NOT NULL, 
+    [TransactieDatum] DATETIME NOT NULL, 
+    CONSTRAINT [FK_AandeelId] FOREIGN KEY (AandeelId) REFERENCES Aandeel (AandeelId)
+
+)
