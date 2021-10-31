f4107e38ef78bd7fe2f61f9200173f183c054fc6
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-10-31 21:08:31 +0100

    Delete Oef1 directory

--- Oef1/db/dbo.Transactie.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Transacties]
-(
-	[AandeelId] INT NOT NULL PRIMARY KEY IDENTITY (1,1), 
-    [AantalGekocht] INT NOT NULL DEFAULT 0, 
-    [Koers] MONEY NOT NULL, 
-    [TransactieDatum] DATETIME NOT NULL, 
-    CONSTRAINT [FK_AandeelId] FOREIGN KEY (AandeelId) REFERENCES Aandeel (AandeelId)
-
-)
